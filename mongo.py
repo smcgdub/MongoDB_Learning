71c6664892bc7276cb212a16d6472fdf5f336341
smcgdub <stephen_xyz1@hotmail.com>
2021-05-20 20:04:24 +0000

    First commit

---
+++ mongo.py
@@ -0,0 +1,31 @@
+import os
+import pymongo
+if os.path.exists("env.py"):
+    import env
+
+MONGO_URI = os.environ.get("MONGO_URI")
+DATABASE = "myFirstDB"
+COLLECTION = "famous_people"
+
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Mongo is connected")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MongoDB: %s") % e
+
+
+conn = mongo_connect(MONGO_URI)
+
+coll = conn[DATABASE][COLLECTION]
+
+documents = coll.find()
+
+new_doc = {"first": "douglas", "last": "adams", "dob": "11/03/1952", "hair_colour": "grey", "occupation": "writer", "nationality": "british"}
+
+coll.insert(new_doc)
+
+for doc in documents:
+    print(doc)
